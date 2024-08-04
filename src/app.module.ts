7726151fa57cc0e3b3daa639391d4d3f741b9d8f
Mayamee <ponyashcat228@gmail.com>
2024-08-05 00:39:08 +0300

    add env and db docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, AuthModule, TopPageModule, ReviewModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule,
+    AuthModule,
+    TopPageModule,
+    ReviewModule,
+  ],
 })
 export class AppModule {}
